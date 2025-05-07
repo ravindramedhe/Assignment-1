3cc983970ae9409741f8382e34f3b0162a80223e
ravindramedhe <102851076+ravindramedhe@users.noreply.github.com>
2025-05-07 15:58:25 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,23 @@
+#Task_1
+num1 = int (input('Enter the first number:'))
+
+num2 = int (input('Enter the second number:'))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+
+#Task_2
+first_name = input ('Enter your first name: ')
+
+last_name = input (' Enter your last name: ')
+
+full_name = first_name + " " +last_name
+
+print(f'\n Hello, {full_name}! Welcome to the Python program.')
